e1a8a4cfbefd3675fe433858e84f2d6c9bef1ba9
mcalisadeghi <ali.ke970@gmail.com>
2023-09-16 23:31:59 +0330

    add some praperty

--- lib/main.dart
+++ lib/main.dart
@@ -28,26 +28,38 @@ class MyHomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      bottomNavigationBar: GNav(
-        tabs: [
-          GButton(
-            icon: Icons.home,
-            text: 'Home',
+    return Scaffold(
+      bottomNavigationBar: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: GNav(
+          gap: 10,
+          backgroundColor: Colors.black,
+          color: Colors.white,
+          activeColor: Colors.white,
+          tabBackgroundColor: Colors.grey.shade800,
+          padding: EdgeInsets.symmetric(
+            vertical: 20,
+            horizontal: 15.0,
           ),
-          GButton(
-            icon: Icons.favorite,
-            text: 'Likes',
-          ),
-          GButton(
-            icon: Icons.search,
-            text: 'Search',
-          ),
-          GButton(
-            icon: Icons.settings,
-            text: 'Settings',
-          ),
-        ],
+          tabs: [
+            GButton(
+              icon: Icons.home,
+              text: 'Home',
+            ),
+            GButton(
+              icon: Icons.favorite,
+              text: 'Likes',
+            ),
+            GButton(
+              icon: Icons.search,
+              text: 'Search',
+            ),
+            GButton(
+              icon: Icons.settings,
+              text: 'Settings',
+            ),
+          ],
+        ),
       ),
     );
   }
